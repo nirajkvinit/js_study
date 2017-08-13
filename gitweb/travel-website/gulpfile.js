2389c526151f36bec1fdacd4c633c34d3598c574
Niraj Kumar <nirajkvinit@gmail.com>
2017-08-13 16:11:39 +0530

    ch10 completed

--- gitweb/travel-website/gulpfile.js
+++ gitweb/travel-website/gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp'),
-	watch = require('gulp-watch');
+	watch = require('gulp-watch'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested');
 
 gulp.task('default', function(){
 	console.log('testing default task');
 });
 
 gulp.task('styles', function(){
-	console.log('sass/less compilation test');
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles/'));
 });
 
 gulp.task('watch', function(){
