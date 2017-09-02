ce601092d01b6189e97c96b8c69a9417694bcb76
Niraj Kumar <nirajkvinit@gmail.com>
2017-09-02 22:21:33 +0530

    gulp and browser-sync implemented.

---
+++ jsmod/ch3/js/app.js
@@ -0,0 +1,14 @@
+(function(){
+	console.log('Starting MultiMath with an IIFE');
+	document.getElementById('startGame').addEventListener('click', function() {
+		player.setName(document.getElementById('playername').value);
+		game.printGame();
+	});
+
+	document.getElementById('calculate').addEventListener('click', function() {
+		game.calculateScore();
+	});
+
+	document.getElementById('problemCount').value = game.getProblemCount();
+
+})();
