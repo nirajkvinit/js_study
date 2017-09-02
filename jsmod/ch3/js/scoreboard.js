bed0011714ac0be791c1aef897c1eec20e5048d2
Niraj Kumar <nirajkvinit@gmail.com>
2017-09-03 00:32:10 +0530

    ch3-5 completed

---
+++ jsmod/ch3/js/scoreboard.js
@@ -0,0 +1,26 @@
+var scoreboard = function() {
+
+	var results = [];
+
+	function addResult(newResult) {
+		results.push(newResult);
+	}
+
+	function updateScoreBoard() {
+		var output = '<h2>Scoreboard</h2>';
+		for (var index = 0; index < results.length; index++) {
+			var result = results[index];
+			output += '<h4>';
+			output += result.name + ': ' + result.score + '/' + result.problems + 'for factor ' + result.factor;
+			output += '</h4>'
+		}
+
+		var scoresElement = document.getElementById('scores');
+		scoresElement.innerHTML = output;
+	}
+	
+	return {
+		addResult : addResult,
+		updateScoreBoard: updateScoreBoard
+	}
+}();
