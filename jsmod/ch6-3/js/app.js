9fc1035165c1c96eb11f11d9d4b76ba9307b8887
Niraj Kumar <nirajkvinit@gmail.com>
2017-09-21 14:56:09 +0530

    Webpack config for ES2015

---
+++ jsmod/ch6-3/js/app.js
@@ -0,0 +1,15 @@
+import assignPlayerName from './player.js';
+import { printGame, calculateScore, getProblemCount } from './game.js';
+
+console.log('Loading with SystemJS');
+
+document.getElementById('startGame').addEventListener('click', function() {
+	assignPlayerName(document.getElementById('playername').value);
+	printGame();
+});
+
+document.getElementById('calculate').addEventListener('click', function() {
+	calculateScore();
+});
+
+document.getElementById('problemCount').value = getProblemCount();
